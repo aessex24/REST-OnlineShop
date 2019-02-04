ed7fb55c940cd969da2b8b32d7b10dc541329853
Andrew Essex <aessex@tmcdesign.com>
2019-02-04 08:17:12 -0700

    add user model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
@@ -43,12 +44,15 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect('mongodb+srv://aessex_24:2s5j9Q61uPVA1BuG@cluster0-ochml.mongodb.net/messages?retyWrites=true')
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const { body } = require('express-validator/check');
+const router = express.Router();
+const User = require('../models/User');
+
+const authCtrl = require('../controllers/auth');
+
+router.put('/signup', [
+  body('email').isEmail().withMessage('Please enter a valid email.')
+  .custom((value, { req }) => {
+    return User.findOne({email: value}).then(userDoc => {
+      if(userDoc) {
+        return Promise.reject('Email address already exists');
+      }
+    });
+  })
+  .normalizeEmail(),
+  body('password').trim().isLength({min: 8}),
+  body('name').trim().not().isEmpty()
+], authCtrl.signup);
+
+module.exports = router;
